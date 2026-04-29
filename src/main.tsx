e5eb7dd79b1e61f2069bd9fb7291bc4d29614553
orangescrypto-jpg <orangescrypto@gmail.com>
2026-04-29 17:48:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
-
-// Prevent flash of wrong theme on load
-try {
-  const stored = localStorage.getItem('tetherng-theme');
-  if (stored) {
-    const parsed = JSON.parse(stored);
-    if (parsed?.state?.theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    }
-  }
-} catch { /* ignore */ }
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
